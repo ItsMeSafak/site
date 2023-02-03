b89d860c1d7f863ecac41a7cc11b5e1fa95f7e83
Safak Inan <safak.inan61@hotmail.com>
2023-02-04 00:14:04 +0100

    Issue with SVG's

--- components/hamburger.tsx
+++ components/hamburger.tsx
@@ -1,6 +1,4 @@
 import { ReactElement } from 'react';
-import HamburgerIcon from '../public/hamburger.svg';
-import CloseIcon from '../public/close.svg'
 
 interface HamburgerProps {
     open: boolean;
@@ -8,11 +6,36 @@ interface HamburgerProps {
 
 export const Hamburger = ({open}: HamburgerProps): ReactElement =>
   <>
-  {open ? (
-      <CloseIcon />
-    ) : (
-      <HamburgerIcon />
-    )}
+    {open ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          className="h-6 w-6"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      )}
   </>
 
 export default Hamburger;
