adea09ae5e01ef96d7f864b669fdf4c441428388
Safak Inan <safak.inan61@hotmail.com>
2023-02-04 00:24:24 +0100

    Font issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ import { SEO } from '../components/seo';
 export const Document = () => 
     <Html>
       <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <SEO />
       </Head>
       <body>
