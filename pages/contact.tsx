bdfb21e6126f250d027487101f0e3eab9d81aece
Safak Inan <safak.inan61@hotmail.com>
2023-03-09 19:49:33 +0100

    Adjusted column issue

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -16,7 +16,7 @@ export const ContactPage = ({meData, pageData}: PageProps): ReactElement => {
     
     return (
         <div className='flex flex-col items-center text-center p-10 lg:w-6/12'>
-            <h1 className='text-4xl lg:text-6xl py-2 lg:px-7 font-bold bg-blue'>{title}</h1>
+            <h1 className='text-4xl lg:text-6xl py-2 px-1 lg:px-7 font-bold bg-blue'>{title}</h1>
             <p className='py-2 lg:w-8/12 mb-5'>{description}</p>
             <a className='bg-blue shadow-md py-2 px-5 rounded-3xl' href={mail}>{button}</a>
         </div>
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -23,7 +23,7 @@ export const SkillsPage = ({meData, pageData}: PageProps): ReactElement => {
     return (
         <div className='flex items-center flex-col w-full lg:p-10 lg:w-6/12'>
             <h1 className='text-4xl lg:text-6xl py-2 px-10 font-bold bg-blue mb-10'>{title}</h1>
-            <div className='grid grid-auto-columns gap-5'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
                 {renderSkills()}
             </div>
         </div>
